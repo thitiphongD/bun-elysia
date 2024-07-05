376de550f67d5d626d384ee91e00a6eba44fa680
Thitiphong Srisavat <80618380+thitiphongD@users.noreply.github.com>
2024-07-05 17:17:22 +0700

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,63 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
+import { swagger } from "@elysiajs/swagger";
+import { Client } from "pg";
+import { cors } from "@elysiajs/cors";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const client = new Client({
+  host: Bun.env.DB_HOST,
+  port: parseInt(Bun.env.DB_PORT || "5432", 10), // Default port 5432 if DB_PORT is undefined
+  user: Bun.env.DB_USER,
+  password: Bun.env.DB_PASSWORD,
+  database: Bun.env.DB_NAME,
+});
 
+const connectDB = async () => {
+  try {
+    await client.connect();
+    console.log("Connected to PostgreSQL successfully");
+  } catch (err) {
+    console.error("Connection error", err);
+    process.exit(1); // Exit the process if connection fails
+  }
+};
+
+connectDB();
+
+const app = new Elysia();
+app.use(cors());
+
+app.get("/", () => "hello 🦊");
+
+app.get("/users", async () => {
+  try {
+    const result = await client.query("SELECT * FROM health_check");
+    return result.rows;
+  } catch (error) {
+    console.error("Error executing PostgreSQL query:", error);
+    return "Error";
+  }
+});
+
+app.post(
+  "/user",
+  async ({ body: { name }, error }) => {
+    if (name === "Otto") return error(400, "Bad Request");
+    try {
+      await client.query(`INSERT INTO health_check (name) VALUES ($1)`, [name]);
+      return { message: "User profile created successfully", name };
+    } catch (error) {
+      console.error("Error inserting into PostgreSQL:", error);
+      return error;
+    }
+  },
+  {
+    body: t.Object({
+      name: t.String(),
+    }),
+  }
+);
+
+app.listen(8080);
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
